66ed0ac888e422564ea202b870c02e89ce4bd60f
DeepakGupta007 <deepakgupta090998@gmail.com>
2023-06-28 19:15:06 +0530

    moved the GKE cluster resource to sub directory

---
+++ cluster/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+    # alias = "inherited"
+    # region = module.gcp_module.region
+    # zone = module.gcp_module.zone
+    # credentials = module.gcp_module.credentials
+}
--- variables.tf
+++ variables.tf
@@ -9,33 +9,3 @@ variable "project_name" {
 
 variable "creds" {
 }
-
-variable "cluster_name" {
-  description = "The name for the GKE cluster"
-  default     = "my-cluster"
-}
-
-variable "env_name" {
-  description = "The environment for the GKE cluster"
-  default     = "test"
-}
-
-variable "network" {
-  description = "The VPC network created to host the cluster in"
-  default     = "gke-network"
-}
-
-variable "subnetwork" {
-  description = "The subnetwork created to host the cluster in"
-  default     = "gke-subnet"
-}
-
-variable "ip_range_pods_name" {
-  description = "The secondary ip range to use for pods"
-  default     = "ip-range-pods"
-}
-
-variable "ip_range_services_name" {
-  description = "The secondary ip range to use for services"
-  default     = "ip-range-services"
-}
